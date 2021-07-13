7a66f42bb7e7fc943edb3fac4800c8e350adabb9
Honorio <hmneton@gmail.com>
2021-07-13 18:05:36 +0000

    update

--- app/precache-manifest.55d6951a4db401b71afb9bb0c06c82d3.js
+++ app/precache-manifest.55d6951a4db401b71afb9bb0c06c82d3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f9cf01624a724cc54106c450b9d0c360",
+    "revision": "c2b906c2511f91c370d2e5cfb2c38ac3",
     "url": "/index.html"
   },
   {
-    "revision": "738f78a2dd428a9d714c",
+    "revision": "d42c52ceefc5abec0af2",
     "url": "/static/css/main.00206758.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.2deedd0c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "738f78a2dd428a9d714c",
-    "url": "/static/js/main.2d98cce0.chunk.js"
+    "revision": "d42c52ceefc5abec0af2",
+    "url": "/static/js/main.8f14d877.chunk.js"
   },
   {
     "revision": "334bc887324152744294",
