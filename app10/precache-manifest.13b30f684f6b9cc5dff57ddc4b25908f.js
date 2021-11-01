acc8d0a68bbd13bf5e4a9313f3b67841eee6fe7d
Honorio <hmneton@gmail.com>
2021-10-31 21:43:30 -0300

    update

---
+++ app10/precache-manifest.13b30f684f6b9cc5dff57ddc4b25908f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a8997a94d88cea911caf0725be37ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83bf3fb75aa35c388147",
+    "url": "/static/css/main.d217bda6.chunk.css"
+  },
+  {
+    "revision": "8a957fa1835153dccba1",
+    "url": "/static/js/2.616105f9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.616105f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83bf3fb75aa35c388147",
+    "url": "/static/js/main.636f8cd4.chunk.js"
+  },
+  {
+    "revision": "334bc887324152744294",
+    "url": "/static/js/runtime-main.8ca5ca62.js"
+  },
+  {
+    "revision": "da7d5c567ae4db7f5518578a54a95212",
+    "url": "/static/media/logo.da7d5c56.jpeg"
+  }
+]);
