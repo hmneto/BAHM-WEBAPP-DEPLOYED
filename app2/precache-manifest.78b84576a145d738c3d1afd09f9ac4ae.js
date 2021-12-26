073b12b1503e060484967fbc0c424c9200cb0931
Honório <hmneton@gmail.com>
2021-12-26 20:18:13 -0300

    update

---
+++ app2/precache-manifest.78b84576a145d738c3d1afd09f9ac4ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5293b04baa2faaf026bf22fd8028315a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcc2a2f123518a051f0f",
+    "url": "/static/css/main.4f2bc79d.chunk.css"
+  },
+  {
+    "revision": "f82024d5153e5685d98c",
+    "url": "/static/js/2.62f0b86d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.62f0b86d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc2a2f123518a051f0f",
+    "url": "/static/js/main.94546f7d.chunk.js"
+  },
+  {
+    "revision": "334bc887324152744294",
+    "url": "/static/js/runtime-main.8ca5ca62.js"
+  }
+]);
