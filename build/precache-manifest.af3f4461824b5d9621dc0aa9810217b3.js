75bae10c313cd2cd75877c7588f7f4bd6c7fb17f
hmneto <hmneton@gmail.com>
2022-08-04 06:14:37 -0300

    update

---
+++ build/precache-manifest.af3f4461824b5d9621dc0aa9810217b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f526d950f7f5e5e0826c76afde2aee1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37cb9d7cb68070a6f964",
+    "url": "/static/css/main.978d7190.chunk.css"
+  },
+  {
+    "revision": "1358434f4d3e02bfcb6a",
+    "url": "/static/js/2.cd44b69c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.cd44b69c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37cb9d7cb68070a6f964",
+    "url": "/static/js/main.57bc0c52.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.57bc0c52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "334bc887324152744294",
+    "url": "/static/js/runtime-main.8ca5ca62.js"
+  }
+]);
