62d2a55135bd8f5b5b9c2b2e3be2aa2c09803bd7
hmneto <hmneton@gmail.com>
2022-08-08 18:42:21 -0300

    update

---
+++ app/precache-manifest.30428b9aa950b0a8156a0baffa840b3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae502519f0757e97fb2a852d3f8ffbed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd7b1e8bc80626f513a3",
+    "url": "/static/css/main.978d7190.chunk.css"
+  },
+  {
+    "revision": "1358434f4d3e02bfcb6a",
+    "url": "/static/js/2.cd44b69c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.cd44b69c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd7b1e8bc80626f513a3",
+    "url": "/static/js/main.dcababcd.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.dcababcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "334bc887324152744294",
+    "url": "/static/js/runtime-main.8ca5ca62.js"
+  }
+]);
--- app2/service-worker.js
+++ app2/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30428b9aa950b0a8156a0baffa840b3b.js"
+  "/precache-manifest.a2d053f928820873543ff51c69d6c4d1.js"
 );
 
 self.addEventListener('message', (event) => {
