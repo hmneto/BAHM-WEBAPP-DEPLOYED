fc9464f990e66725a430033e64493f8a3cd9aea1
hmneto <hmneton@gmail.com>
2023-02-12 23:21:22 -0300

    update

--- app/js/ponto/pontoLatLng.js
+++
@@ -1,7 +0,0 @@
-
-function fillInputsLatLng() {
-    const { lat, lng } = getLatLong()
-    console.log(lat,lng)
-    document.getElementById('latitude').value = lat.toFixed(6)
-    document.getElementById('longitude').value = lng.toFixed(6)
-}
