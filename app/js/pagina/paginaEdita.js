ba0a39965df255d956222c60326bf3314696d7b7
hmneto <hmneton@gmail.com>
2023-02-12 23:22:43 -0300

    update

---
+++ app/js/pagina/paginaEdita.js
@@ -0,0 +1,73 @@
+async function paginaEditaInteracao(id) {
+    document.getElementById('salvar').hidden = true
+    document.getElementById('editar').hidden = false
+    const detalhesPagina = await httpGet("/pagina/Pagina?PaginaId=" + id)
+    console.log(detalhesPagina)
+
+
+    const listaContatoSelecionado = document.getElementById('listaContatoSelecionado')
+    listaContatoSelecionado.appendChild(document.createElement('br'))
+
+    const listaSiteSelecionado = document.getElementById('listaSiteSelecionado')
+    listaSiteSelecionado.appendChild(document.createElement('br'))
+
+
+    for (let i = 0; i < detalhesPagina.listContatoDto.length; i++) {
+        montaPaginaContato(detalhesPagina.listContatoDto[i])
+    }
+
+
+    for (let i = 0; i < detalhesPagina.listSiteDto.length; i++) {
+        montaPaginaSite(detalhesPagina.listSiteDto[i])
+    }
+
+
+    
+
+    document.getElementById('idPagina').value = detalhesPagina.idPagina
+    document.getElementById('nomePagina').value = detalhesPagina.nomePagina
+    document.getElementById('enderecoPagina').value = detalhesPagina.enderecoPagina
+    document.getElementById('concessionaria').value = detalhesPagina.concessionaria.nomeConcessionaria
+    document.getElementById('idConcessionaria').value = detalhesPagina.concessionaria.idConcessionaria
+
+
+}
+
+async function paginaEdita() {
+
+    const collection = document.getElementsByClassName("contatoSelecionadoClass")
+    const ListContatoDto = []
+    for (let i = 0; i < collection.length; i++) {
+        ListContatoDto.push({ IdContato: collection[i].value })
+    }
+
+
+
+    const collection0 = document.getElementsByClassName("siteSelecionadoClass")
+    const ListSiteDto = []
+    for (let i = 0; i < collection0.length; i++) {
+        console.log(collection0[i].value)
+        ListSiteDto.push({ IdSite: collection0[i].value })
+    }
+
+    const pagina = {
+        "IdPagina": document.getElementById('idPagina').value,
+        "NomePagina": document.getElementById('nomePagina').value,
+        "EnderecoPagina": document.getElementById('enderecoPagina').value,
+        "ConcessionariaId": document.getElementById('idConcessionaria').value,
+        ListContatoDto,
+        ListSiteDto
+
+    }
+
+    await httpPut("/Pagina/Edita", pagina).then(x => {
+
+        if (x) {
+            openView("listaPagina")
+        } else {
+            alert("erro")
+        }
+    });
+}
+
+
