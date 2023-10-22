c5508a9ce44dff3727d3ca444bf0f68d33d39b6a
Honório <hmneton@gmail.com>
2023-10-22 12:00:02 -0300

    ok

--- app/index.js
+++ app/index.js
@@ -12,7 +12,7 @@ let dadosPonto = null
 const pontosMaps = []
 
 function openViewTable(page, edita, id) {
-  fetch(`pages70/${page}.html`)
+  fetch(`pages71/${page}.html`)
     .then(function (response) {
       return response.text();
     })
@@ -41,7 +41,7 @@ function openView(page, edita, id) {
     dadosPonto = null
     latLgnii = null
   }
-  fetch(`pages70/${page}.html`)
+  fetch(`pages71/${page}.html`)
     .then(function (response) {
       return response.text();
     })
